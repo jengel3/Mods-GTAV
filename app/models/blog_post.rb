409b6a19898236d3e6f077f7d337208e8ea60f45
Jake <jake0oo0dev@gmail.com>
2015-04-26 20:23:59 -0500

    Readd creator

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -11,4 +11,9 @@
 #
 
 class BlogPost < ActiveRecord::Base
+  belongs_to :author, class_name: "User"
+
+  validates :body, presence: true
+  validates :author_id, presence: true
+  validates :title, presence: true
 end
