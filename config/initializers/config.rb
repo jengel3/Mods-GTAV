c7c0c63bcc10e53c6fa8c762bc5c2a10ec118ea2
Jake <jake0oo0dev@gmail.com>
2015-04-27 20:30:08 -0500

    Fix initializer spacing

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -9,5 +9,5 @@ end
 require 'elasticsearch/model'
 require 'elasticsearch/transport'
 if ENV['BONSAI_URL']
- Elasticsearch::Model.client = Elasticsearch::Client.new url: ENV['BONSAI_URL']
+  Elasticsearch::Model.client = Elasticsearch::Client.new url: ENV['BONSAI_URL']
 end
